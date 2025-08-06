98cad7b45450e71898bf7210b5cfe25958e37158
ElavazhaganSF4422 <161409140+ElavazhaganSF4422@users.noreply.github.com>
2025-08-06 16:58:45 +0530

    KB-739846 - How to achieve Excel-like text alignment in WInForms GridControl

---
+++ ExcelLikeTextAlignment_CS/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows.Forms;
+
+namespace ExcelLikeTextAlignment_CS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            gridControl1.RowCount = 10;
+            gridControl1.ColCount = 6;
+            gridControl1.ExcelLikeAlignment = true;
+            for (int i = 1; i <= gridControl1.RowCount; i++)
+            {
+                for (int j = 0; j <= gridControl1.ColCount/2; j++)
+                {
+                    gridControl1[i, j].CellValue = i+j;
+                }
+            }
+            for (int i = 1; i <= gridControl1.RowCount; i++)
+            {
+                for (int j = gridControl1.ColCount / 2+1; j <= gridControl1.ColCount; j++)
+                {
+                    gridControl1[i, j].CellValue = "Data";
+                }
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # How-to-achieve-Excel-like-text-alignment-in-WinForms-GridControl
-This demos shows how to achieve Excel-like text alignment in WinForms GridControl?
+
+By default, text alignment in grid control is right aligned for both alphabets and numeric. In order to achieve excel like alignment, make use of [WinForms Grid](https://www.syncfusion.com/winforms-ui-controls/grid-control) ExcelLikeAlignment property. So, the string values will be aligned left side and numeric values will be aligned right side of the cell.
+
+Code Snippet
+
+C#
+```
+this.gridControl1.ExcelLikeAlignment = true;
+```
+
+VB
+```
+Me.gridControl1.ExcelLikeAlignment = True
+```
+
+Screenshot
+
+![Showing text alignment in GridControl](ExcelLikeTextAlignment.png)
+
+[View sample in GitHub](https://github.com/SyncfusionExamples/How-to-achieve-Excel-like-text-alignment-in-WinForms-GridControl)
