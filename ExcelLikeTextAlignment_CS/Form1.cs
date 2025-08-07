d47f8c468505746c3929fa04ec6bbe433b7740d2
ElavazhaganSF4422 <161409140+ElavazhaganSF4422@users.noreply.github.com>
2025-08-07 17:00:46 +0530

    How to achieve Excel-like text alignment in WInForms GridControl

--- ExcelLikeTextAlignment_CS/Form1.cs
+++ ExcelLikeTextAlignment_CS/Form1.cs
@@ -7,15 +7,15 @@ namespace ExcelLikeTextAlignment_CS
         public Form1()
         {
             InitializeComponent();
-            gridControl1.RowCount = 10;
-            gridControl1.ColCount = 6;
-            gridControl1.ExcelLikeAlignment = true;
+            this.gridControl1.RowCount = 10;
+            this.gridControl1.ColCount = 6;
+            this.gridControl1.ExcelLikeAlignment = true;
             //Fill first half of columns with numeric values
             for (int i = 1; i <= gridControl1.RowCount; i++)
             {
                 for (int j = 0; j <= gridControl1.ColCount/2; j++)
                 {
-                    gridControl1[i, j].CellValue = i+j;
+                    this.gridControl1[i, j].CellValue = i+j;
                 }
             }
             //Fill second half of columns with "Data"
@@ -23,7 +23,7 @@ namespace ExcelLikeTextAlignment_CS
             {
                 for (int j = gridControl1.ColCount / 2+1; j <= gridControl1.ColCount; j++)
                 {
-                    gridControl1[i, j].CellValue = "Data";
+                    this.gridControl1[i, j].CellValue = "Data";
                 }
             }
         }
--- ExcelLikeTextAlignment_VB/Form1.vb
+++ ExcelLikeTextAlignment_VB/Form1.vb
@@ -2,21 +2,21 @@
     Public Sub New()
         InitializeComponent()
 
-        GridControl1.RowCount = 10
-        GridControl1.ColCount = 6
-        GridControl1.ExcelLikeAlignment = True
+        Me.GridControl1.RowCount = 10
+        Me.GridControl1.ColCount = 6
+        Me.GridControl1.ExcelLikeAlignment = True
 
         ' Fill first half of columns with numeric values
         For i As Integer = 1 To GridControl1.RowCount
             For j As Integer = 0 To GridControl1.ColCount \ 2
-                GridControl1(i, j).CellValue = i + j
+                Me.GridControl1(i, j).CellValue = i + j
             Next
         Next
 
         ' Fill second half of columns with "Data"
         For i As Integer = 1 To GridControl1.RowCount
             For j As Integer = (GridControl1.ColCount \ 2) + 1 To GridControl1.ColCount
-                GridControl1(i, j).CellValue = "Data"
+                Me.GridControl1(i, j).CellValue = "Data"
             Next
         Next
     End Sub
