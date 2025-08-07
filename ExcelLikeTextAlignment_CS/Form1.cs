ce7f8a53b662372b53841cc34276717d8e4fdc91
ElavazhaganSF4422 <161409140+ElavazhaganSF4422@users.noreply.github.com>
2025-08-07 15:55:37 +0530

    How to achieve Excel-like text alignment in WInForms GridControl

--- ExcelLikeTextAlignment_CS/Form1.cs
+++ ExcelLikeTextAlignment_CS/Form1.cs
@@ -10,6 +10,7 @@ namespace ExcelLikeTextAlignment_CS
             gridControl1.RowCount = 10;
             gridControl1.ColCount = 6;
             gridControl1.ExcelLikeAlignment = true;
+            //Fill first half of columns with numeric values
             for (int i = 1; i <= gridControl1.RowCount; i++)
             {
                 for (int j = 0; j <= gridControl1.ColCount/2; j++)
@@ -17,6 +18,7 @@ namespace ExcelLikeTextAlignment_CS
                     gridControl1[i, j].CellValue = i+j;
                 }
             }
+            //Fill second half of columns with "Data"
             for (int i = 1; i <= gridControl1.RowCount; i++)
             {
                 for (int j = gridControl1.ColCount / 2+1; j <= gridControl1.ColCount; j++)
